2a77ee0c52d4fa6615fe5d99f16ab356dffb7061
Jonathan Marin <44934923+iTzJonathanxD@users.noreply.github.com>
2025-02-12 15:22:05 -0500

    feat: implementacion de servicios y controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [DatabaseModule, UserModule],
