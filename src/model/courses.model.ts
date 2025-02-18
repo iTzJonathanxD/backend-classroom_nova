16a62c4e1bf251b981375f8f80cbd42237d58039
Alexis Leon Reyes <83489240+ToshioDev@users.noreply.github.com>
2025-02-18 14:31:42 -0600

    feat: add more parameters to courses model

--- src/model/courses.model.ts
+++ src/model/courses.model.ts
@@ -2,6 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { Live } from './live.model'; 
 import { User } from './user.model';
+import { CategoryCourse } from './categorycourse.model';
 
 export type CourseDocument = Course & Document;
 
@@ -12,6 +13,24 @@ export class Course {
 
   @Prop({ required: true })
   descripion: string;
+
+  @Prop({ required: false })
+  imageUrl: string;
+
+  @Prop({ required: true, enum: ['Principiante', 'Intermedio', 'Avanzado'] })
+  difficulty: string;
+
+  @Prop({ required: true })
+  duration: string;
+
+  @Prop({ required: true, min: 0, max: 5, default: 0 })
+  rating: number;
+
+  @Prop({ required: true, min: 0 })
+  price: number;
+
+  @Prop({ type: String, ref: 'CategoryCourse', required: true })
+  categoryId: CategoryCourse;
 
   @Prop({ type: [{ type: String, ref: 'User' }] })
   docenteId: User[];
